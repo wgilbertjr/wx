1494a5a5df205b35a03d7714153e66c50dae62cd
wgilbertjr <wgilbertjr@gmail.com>
2024-11-14 08:45:52 -0500

    Update

--- wx/wx_mods.py
+++ wx/wx_mods.py
@@ -2,36 +2,8 @@ import json
 import requests
 import sys
 
-# https://api.weatherapi.com/v1/astronomy.json?key=03b8e7b5c6e74f02bfe115319242408&q=21742
-# https://api.weatherapi.com/v1/current.json?key=03b8e7b5c6e74f02bfe115319242408&q=21742
-#
-# To Do:
-# add command parameters to command
-# error checking for api calls
-# config file or commandline arguments?
-# logging for errors, etc
-
-def main():
-    # move these to config file.
-    wx_token = '03b8e7b5c6e74f02bfe115319242408'
-    wx_base_url = 'https://api.weatherapi.com/v1/'
-    wx_astro_api = 'astronomy.json'
-    wx_current_api = 'current.json'
-    
-    # check valid US zipcode.
-    try:
-        location = sys.argv[1]
-    except IndexError:
-        location = '21742' # input('US Zipcode: ')
         
-    try:
-        astro_data(wx_base_url, wx_astro_api, wx_token, location)
-        current_wx(wx_base_url, wx_current_api, wx_token, location)
-        
-    except:
-        print('What happened?')
-        
-def get_wind(r_json):
+def get_wind_data(r_json):
     
     wind_speed_mph = r_json['current']['wind_mph']
     wind_speed_kph = r_json['current']['wind_kph']
@@ -46,7 +18,7 @@ def current_wx(wx_base_url, wx_current_api, wx_token, location):
     parameters = {'q': location}
     
     r_json = getWxJson(url, parameters)
-    #return r_json
+    # print(r_json)
     print(f'Current Conditions: {r_json['current']['condition']['text']}')
     print(f'Temperature: {r_json['current']['temp_f']} {u'\N{DEGREE SIGN}'}F ({r_json['current']['temp_c']} {u'\N{DEGREE SIGN}'}C)')
     print(f'Barometer: {r_json['current']['pressure_in']}in ({r_json['current']['pressure_mb']} mb)')
@@ -70,5 +42,3 @@ def getWxJson(url, parameters):
     r_json = json.loads(r.text)
     return r_json
 
-if __name__ == '__main__':
-    main()
